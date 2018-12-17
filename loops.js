4e8fe06ceb2648e31a3af90001b6152454572d98
Justin Hirata <jlhirata88@aol.com>
2018-12-17 19:50:56 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -21,7 +21,7 @@ function whileLoop(n){
   
 function doWhileLoop(array){
  function incrementVariable() {
-    return Math.random() <= 0.5
+    return Math.random() >= 0.5
   }
    do {
     array.pop();
